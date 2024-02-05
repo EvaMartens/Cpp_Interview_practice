5941fa2e2451acc5efcf77710901067379927d21
Eva Martens <144374246+EvaMartens@users.noreply.github.com>
2024-02-05 18:54:30 +0000

    Update LeetCodeMedium.cpp

--- LeetCodeMedium.cpp
+++ LeetCodeMedium.cpp
@@ -2,6 +2,23 @@
 // Created by epm04 on 05/02/2024.
 //
 
+/** Problem Description:
+
+Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that 
+each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the 
+result be placed in the first part of the array nums. More formally, if there are k elements after 
+removing the duplicates, then the first k elements of nums should hold the final result. It does not 
+matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place 
+with O(1) extra memory.
+
+**/
+
 #include <iostream>
 #include <vector>
 
@@ -10,7 +27,7 @@ class Solution {
 public:
     int removeDuplicates(std::vector<int>& nums) {
         //-------------------------------
-
+        //Constraints:
         //1) element allowed at most twice
         //2) order needs to be maintained
         //3) return k elements
@@ -39,9 +56,9 @@ public:
 
         //array size at least 3
         // 1 1 1
-        //     ^
+        //   ^
 
-        for(int i = 1; i < nums.size(); i++){ //nOW = 1
+        for(int i = 1; i < nums.size(); i++){ //from second position to end
             if(nums[i] == nums[i - 1]){
                 nOW++;
                 std::cout << "Nums i is -> " << nums[i] << " and nOW is " << nOW << std::endl;
@@ -49,36 +66,21 @@ public:
                     while(nums[i] == nums[i-1]){
                         std::cout << nums[i] << " got erased" << std::endl;
                         nums.erase(nums.begin()+i-1);
-                        for(int i : nums)
-                            std::cout << i << " ";
-                        if(i == nums.size()){
+                        if(i == nums.size()){ //makes sure we don't get index out of bounds error when deleting second to last element
                             break;
                         }
                     }
                     nOW = 1;
                 }
-                k++;
                 std::cout<< "1) k is " << k << std::endl;
             } else {
                 nOW = 1;
-                k++;
                 std::cout<< "2) k is " << k << std::endl;
             }
         }
-
-        std::cout << k << std::endl;
+        
         k = nums.size();
         return k;
-
-        //if next element is the same as previous, nOE++
-        //if nOW > 2, delete local element
-        //grab next element and copy value into current position
-
-        //return k elements
-
-
-
-
 
     }
 };
